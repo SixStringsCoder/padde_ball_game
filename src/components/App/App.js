5a042059ab22a6a9caa361f4652b266e5e75b899
SixStringsCoder <rshanlon@gmail.com>
2018-08-05 12:56:08 -0700

    Added specs object to handle Globals; shifted some globals to Gameboard state to handle changing score, lives, x and y position

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Gameboard from '../Gameboard/Gameboard';
 
+
 class App extends Component {
   render() {
     return (
--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -2,33 +2,75 @@
 import React, { Component } from 'react';
 import './Gameboard.css';
 
-const width = 800;
-const height = window.innerHeight;
-const ratio = window.devicePixelRatio || 1;
+const specs = {
+  boardWidth: 500,
+  boardHeight: 500,
+  ballRadius: 10,
+  paddleHeight: 20,
+  paddleWidth: 80,
+  rightPressed: false,
+  leftPressed: false,
+  ballColor: "rgba(100, 14, 221, 1)",
+  x_pos: this.boardWidth / 2,
+  y_pos: this.boardHeight - 30,
+  draw_x: 2,
+  draw_y: -2,
+  playGame: true,
+  brickRowCoun:5,
+  brickColumnCoun:6,
+  brickWidt:65,
+  brickHeigh:20,
+  brickPaddin:10,
+  brickOffsetTo:30,
+  brickOffsetLef:30,
+  score: 0,
+  lives: 3
+}
+
 
 class Gameboard extends Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-          screen: {
-              width: width,
-              height: height,
-              ratio: ratio
-          }
-      }
+  state = {
+    x_pos: specs.boardWidth / 2,
+    y_pos: specs.boardHeight - 30,
+    lives: 3,
+    score: 0,
   }
 
 
+   componentDidMount() {
+      this.drawGameBoard();
+    }
 
-  render() {
-    return (
-      <div id="Gameboard">
-        <canvas ref="canvas"
-             width={ this.state.screen.width * this.state.screen.ratio }
-                height={ this.state.screen.height * this.state.screen.ratio } />
-      </div>
-    );
-  }
+    drawPaddle(ctx) {
+        ctx.beginPath();
+        ctx.rect((specs.boardWidth - specs.paddleWidth) / 2, specs.boardHeight - specs.paddleHeight, specs.paddleWidth, specs.paddleHeight);
+        ctx.fillStyle = "#7BAD17";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+
+    drawBall(ctx) {
+        ctx.beginPath();
+        ctx.arc(250, 470, specs.ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "rgba(100, 14, 221, 1)";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    drawGameBoard() {
+        const ctx = this.refs.canvas.getContext('2d');
+        this.drawBall(ctx);
+        this.drawPaddle(ctx);
+    }
+
+
+
+    render() {
+        return (
+            <canvas ref="canvas" width={specs.boardWidth} height={specs.boardHeight}/>
+        );
+    }
 }
 
 export default Gameboard;
